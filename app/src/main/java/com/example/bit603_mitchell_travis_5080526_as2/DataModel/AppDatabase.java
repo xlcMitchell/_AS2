70c68f0b08cbb8ed7d71b50c8ece0ce5a1e8edcf
Mitchell Travis <Kiwitiendatm@gmail.com>
2025-09-14 13:32:34 +1200

    Database class created

---
+++ app/src/main/java/com/example/bit603_mitchell_travis_5080526_as2/DataModel/AppDatabase.java
@@ -0,0 +1,33 @@
+package com.example.bit603_mitchell_travis_5080526_as2.DataModel;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+@Database(
+        entities = {Users.class,UserFitness.class},
+        version = 1
+)
+@TypeConverters(DataTypeConverter.class)
+
+
+public abstract class AppDatabase extends RoomDatabase {
+    private static AppDatabase database = null;
+
+    public abstract UsersDao usersDao();
+    public abstract UserFitnessDao userFitnessDao();
+
+
+    public static AppDatabase createDatabaseInstance(Context context){
+        if(database == null){
+            database = Room.databaseBuilder(context.getApplicationContext(),
+                    AppDatabase.class,
+                    "database"
+            ).allowMainThreadQueries().build();
+        }
+        return database;
+    }
+}
