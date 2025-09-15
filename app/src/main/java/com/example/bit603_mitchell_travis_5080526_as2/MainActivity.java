779b5dddeb4d7c0800ef7ac1311f77845bd26f32
Mitchell Travis <Kiwitiendatm@gmail.com>
2025-09-15 16:15:15 +1200

    Main activity onclick listeners added

--- app/src/main/java/com/example/bit603_mitchell_travis_5080526_as2/MainActivity.java
+++ app/src/main/java/com/example/bit603_mitchell_travis_5080526_as2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.bit603_mitchell_travis_5080526_as2;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
@@ -15,13 +17,12 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     private ActivityResultLauncher<String[]> locationPermissionRequest;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
 
         locationPermissionRequest = PermissionsHelper.requestLocationPermissions(
                 this,
@@ -36,13 +37,40 @@ public class MainActivity extends AppCompatActivity {
             // Permission already granted to execute code here
 
         } else {
-            // Not granted → launch the request
+            // Not granted so request launched
             locationPermissionRequest.launch(new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.ACCESS_COARSE_LOCATION
             });
         }
 
+        //---Buttons---//
+        Button btnProfile = findViewById(R.id.btnProfile);
+        Button btnLocate = findViewById(R.id.btnLocate);
+        Button btnStepCounter = findViewById(R.id.btnStepCounter);
+
+        //---BUTTON ONCLICK LISTENERS---//
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO start profile activity
+            }
+        });
+
+        btnLocate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               //TODO start locate me activity
+            }
+        });
+
+        btnStepCounter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO Start step counter activity
+            }
+        });
+    }
+
 
     }
-}
